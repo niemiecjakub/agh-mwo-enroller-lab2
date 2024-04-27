da5f1873218e4fd9e96bbcbad95fe7ee17abd682
niemiecjakub <kniemiec3@gmail.com>
2024-04-27 15:28:59 +0200

    fix

--- src/main/java/com/company/enroller/controllers/MeetingRestController.java
+++ src/main/java/com/company/enroller/controllers/MeetingRestController.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.company.enroller.model.Participant;
 import com.company.enroller.persistence.MeetingService;
 
 import java.util.Collection;
