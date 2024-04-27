769d05294357a240e7d07e5c0484067327c1db5b
niemiecjakub <kniemiec3@gmail.com>
2024-04-27 14:08:15 +0200

    Meeting controller with base endpoints

--- src/main/java/com/company/enroller/persistence/MeetingService.java
+++ src/main/java/com/company/enroller/persistence/MeetingService.java
@@ -1,6 +1,7 @@
 package com.company.enroller.persistence;
 
 import com.company.enroller.model.Meeting;
+import com.company.enroller.model.Participant;
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
@@ -22,4 +23,7 @@ public class MeetingService {
 		return query.list();
 	}
 
+	public Meeting findById(Long id) {
+		return this.session.get(Meeting.class, id);
+	}
 }
